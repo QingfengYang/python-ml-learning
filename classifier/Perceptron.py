a1a9e3894d1f005eee758e9643fc0a9b0f8c8d2b
Qingfeng.Yang <sfw2005868@gmail.com>
2018-01-21 18:50:16 +0800

    1. plot decision_regions

--- classifier/Perceptron.py
+++ classifier/Perceptron.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 
+import numpy as np
+
 class Perceptron(object):
     """Perception classifier.
 
@@ -43,8 +45,8 @@ class Perceptron(object):
                 self.w_[1:] += update * xi
                 self.w_[0] = update
                 errors += int(update != 0.0)
-                #print("target: %d, predict: %d, update: %.3f" % (target, predict, update))
-                #print("Wehght: %s" % self.w_)
+                print("target: %d, predict: %d, update: %.3f" % (target, predict, update))
+                print("Wehght: %s" % self.w_)
             self.errors_.append(errors)
         return self
 
@@ -55,6 +57,60 @@ class Perceptron(object):
     def predict(self, xi_vec):
         """Return class label after unit step"""
         return np.where(self.net_input(xi_vec) >= 0.0, 1, -1)
+
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, resolution = 0.02):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
+
+def testPerception(X, y):
+    ppn = Perceptron(eta=0.01, n_iter=10)
+    ppn.fit(X, y)
+
+    # plot to show data
+    #plt.subplot(211)
+    #plt.subplot(211)
+    plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+    plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+    plt.xlabel('petal length')
+    plt.ylabel('sepal length')
+    plt.legend(loc='upper left')
+    plt.show()
+
+    # plot perception
+    #plt.subplot(212)
+    plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+    plt.xlabel('Epochs')
+    plt.ylabel('Number of misclassifications')
+    plt.show()
+
+    # plot decision_regions
+    plot_decision_regions(X, y, classifier=ppn)
+    plt.xlabel('sepal length [cm]')
+    plt.ylabel('petal length [cm]')
+    plt.legend(loc='upper left')
+    plt.show()
+
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
@@ -67,21 +123,5 @@ if __name__ == '__main__':
     y = np.where(y == 'Iris-setosa', -1, 1)
     X = df.iloc[0:100, [0, 2]].values
 
-    ppn = Perceptron(eta=0.1, n_iter=10)
-    ppn.fit(X, y)
+    testPerception(X, y)
 
-    # plot to show data
-    plt.subplot(211)
-    plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
-    plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
-    plt.xlabel('petal length')
-    plt.ylabel('sepal length')
-    plt.legend(loc='upper left')
-    #plt.show()
-
-    # plot perception
-    plt.subplot(212)
-    plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
-    plt.xlabel('Epochs')
-    plt.ylabel('Number of misclassifications')
-    plt.show()
